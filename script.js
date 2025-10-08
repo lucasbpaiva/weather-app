3305a0024c7de8209ce1ccbc9eb87d2e82f60b4c
Lucas Paiva <lpaiva234@gmail.com>
2025-10-08 15:07:25 -0300

    create updateDisplay function

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const APIkey = "Q6MWAXWQTYL4HW4QPLF86VA5M";
-let unitGroup = "metric";
-const urlBase = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+const APIkey    = "Q6MWAXWQTYL4HW4QPLF86VA5M";
+let unitGroup   = "metric";
+const urlBase   = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 let queryParams = `unitGroup=${unitGroup}&key=${APIkey}&contentType=json`;
+let weatherData;
 
 async function getWeatherData(location) {
     let url = `${urlBase}/${location}?${queryParams}`;
-    const response = await fetch(url);
+
+    const response    = await fetch(url);
     const weatherData = await response.json();
 
     const currentTemp = weatherData.currentConditions.temp;
-    const sunrise = weatherData.currentConditions.sunrise;
-    const sunset = weatherData.currentConditions.sunset;
-    const visibility = weatherData.currentConditions.visibility;
-    const windSpeed = weatherData.currentConditions.windspeed;
-    const pressure = weatherData.currentConditions.pressure;
-    const humidity = weatherData.currentConditions.humidity;
-    const feelsLike = weatherData.currentConditions.feelslike;
+    const sunrise     = weatherData.currentConditions.sunrise.slice(0, -3);
+    const sunset      = weatherData.currentConditions.sunset.slice(0, -3);
+    const visibility  = weatherData.currentConditions.visibility;
+    const windSpeed   = weatherData.currentConditions.windspeed;
+    const pressure    = weatherData.currentConditions.pressure;
+    const humidity    = weatherData.currentConditions.humidity;
+    const feelsLike   = weatherData.currentConditions.feelslike;
 
     const processedData = { currentTemp, sunrise, sunset, visibility, windSpeed, pressure, humidity, feelsLike, };
-    console.log(processedData);
+    console.log(weatherData);
+
+    return processedData;
 }
 
 const cityInput = document.querySelector("#cityInput");
@@ -26,10 +30,34 @@ const searchBtn = document.querySelector("#searchBtn");
 
 cityInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-        getWeatherData(cityInput.value);
+        updateDisplay();
     }
 });
 
 searchBtn.addEventListener("click", () => {
-    getWeatherData(cityInput.value);
+    updateDisplay();
 });
+
+function updateDisplay() {
+    weatherData = getWeatherData(cityInput.value); // This is a promise!
+    updateSunriseSunset();
+    updateWindSpeed();
+}
+
+const sunriseDisplay = document.querySelector("#sunriseDisplay");
+const sunsetDisplay  = document.querySelector("#sunsetDisplay");
+
+function updateSunriseSunset() {
+    weatherData.then((data) => {
+        sunriseDisplay.textContent = data.sunrise;
+        sunsetDisplay.textContent = data.sunset;
+    });
+}
+
+const windSpeedVal = document.querySelector("#windSpeedVal");
+
+function updateWindSpeed() {
+    weatherData.then((data) => {
+        windSpeedVal.textContent = data.windSpeed + " km/h";
+    });
+}
